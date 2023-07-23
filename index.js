b4b940266070117aad08bd69edaca1088b1f891a
Pete <petr.stepanenko@gmail.com>
2023-07-22 20:15:28 -0400

    add user creation functionality

--- index.js
+++ index.js
@@ -1,18 +1,48 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require('express');
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+app.use(express.static('public'));
+
+const mongoURI = process.env.MONGO_URI;
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connection.on('error', (err) => {
+  console.log(err);
 });
 
+const userSchema = new mongoose.Schema({
+  username: String,
+});
+const User = mongoose.model('User', userSchema);
 
+const validateUsername = () => (null);
+const selectObjectProperties = (targetObj, fieldsArr) => (
+  Object.keys(targetObj).filter((x) => (fieldsArr.includes(x)))
+    .reduce((accum, field) => Object.assign(accum, { [field]: targetObj[field] }), {})
+);
 
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/views/index.html`);
+});
+app.post('/api/users', (req, res) => {
+  const userName = req.body.username;
+  const newUser = new User();
+  newUser.username = userName;
 
+  newUser.save().then((x) => {
+    const neededProperties = ['username', '_id'];
+    let result = x.toObject();
+    result = selectObjectProperties(result, neededProperties);
+    res.json(result);
+  }, (err) => console.log(err));
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log(`Your app is listening on port ${listener.address().port}`);
+});
